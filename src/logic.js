dc4d70d327938710b8334384932b3ac2ab2bbc31
Tom Webber <t.c.w.webber@gmail.com>
2024-08-11 12:06:41 +0200

    refactor to take more input string

--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,27 @@
-const TODAY = "today";
-const TOMORROW = "today";
-const YESTERADAY = "yesterday";
-const STRINGS = [TODAY, TOMORROW, YESTERADAY];
+const incrDay = (nbDays) => {
+    const date = new Date();
+    date.setDate(date.getDate() + nbDays);
+};
+const incrMonth = (nbMonths) => {
+    const date = new Date();
+    date.setMonth(date.getMonth() + nbMonths);
+};
+
+const STRINGS = {
+    today: new Date(),
+    now: new Date(),
+    tomorrow: incrDay(1),
+    yesterday: incrDay(-1),
+    "next week": incrDay(7),
+    "next month": incrMonth(1),
+    "next year": incrMonth(12),
+    "last week": incrDay(-7),
+    "last month": incrMonth(-1),
+    "last year": incrMonth(-12),
+};
 
 const getStringsIdent = (res, input) => {
-    for (let ident of STRINGS) {
+    for (let [ident, value] of Object.entries(STRINGS)) {
         if (ident.startsWith(input)) {
             res.push(ident);
         }
@@ -13,33 +30,36 @@ const getStringsIdent = (res, input) => {
 
 const getArithmetic = (res, input) => {
     if (!input.startsWith("+") && !input.startsWith("-")) return;
+    if (input == "+" || input == "-") return;
     if (input == "+0" || input == "-0") {
-        res.push(TODAY);
+        res.push("today");
     } else if (input == "+1") {
-        res.push(TOMORROW);
+        res.push("tomorrow");
     } else if (input == "-1") {
-        res.push(YESTERADAY);
+        res.push("yesterday");
     } else {
         const nbStr = input.slice(1);
         try {
             const nbInt = parseInt(nbStr);
-            const today = new Date();
-            today.setDate(today.getDate + nbInt);
-            res.push(today.toLocaleDateString());
+            const date = new Date();
+            date.setDate(date.getDate() + nbInt);
+            const value = date.toLocaleDateString();
+            if (value != "Invalid Date") {
+                res.push(date.toLocaleDateString());
+            }
         } finally {
         }
     }
 };
 
-const getAllPropositions = (input) => {
-    var res = [];
-    getStringsIdent;
-};
-
 export const getPropositions = (input) => {
+    if (!input) return [];
     var res = [];
-    for (let i = 0; i < input.length; i++) {
-        res.push("Hello World");
+    getStringsIdent(res, input);
+    if (input.startsWith("in")) {
+        input = "+" + input.slice(2);
     }
-    return res;
+    getArithmetic(res, input);
+    if (res.length == 0) return undefined;
+    return res.slice(0, Math.min(res.length, 10));
 };
